cb190e5b67dba7a9070ded9f98e92199ce3db569
Abner Ferreira <abner@commits.com.br>
2024-03-30 13:17:46 -0300

    Open browser and accept cookies from Hotmart

--- src/hotmart_course_downloader/main.py
+++ src/hotmart_course_downloader/main.py
@@ -1,2 +1,35 @@
+from asyncio import run
+from configparser import ConfigParser
+from pathlib import Path
+
+from playwright.async_api import async_playwright
+
+
+async def main():
+    config = init_config()
+    pw_instance = await async_playwright().start()
+
+    try:
+        async with (await pw_instance.firefox.launch(headless=False) as browser,
+                    await browser.new_context() as ctx,
+                    await ctx.new_page() as page):
+            await page.goto(config['hotmart.urls']['consumer-portal'])
+            await page.wait_for_url(config['hotmart.urls.patterns']['login-portal'])
+            await page.click(config['hotmart.selectors']['accept-cookies-btn'])
+    finally:
+        await pw_instance.stop()
+
+
+def init_config() -> ConfigParser:
+    config_path = Path('../../config/settings.ini')
+
+    if not config_path.is_file():
+        raise FileNotFoundError(f'Missing file {config_path.resolve()}')
+
+    parser = ConfigParser()
+    parser.read(config_path)
+    return parser
+
+
 if __name__ == '__main__':
-    print('Hello world')
+    run(main())
